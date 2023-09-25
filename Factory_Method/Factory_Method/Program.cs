892241a7ca3d5b02237c90f9a6c5782a8645dcff
Phannghia270895 <107843786+Phannghia270895@users.noreply.github.com>
2023-09-26 00:38:15 +0700

    add

---
+++ Factory_Method/Factory_Method/Program.cs
@@ -0,0 +1,70 @@
+﻿//using Factory_Method;
+
+//string cardType = "MoneyBack";
+
+//CreditCard cardDetails = null;
+
+////Based of the CreditCard Type we are creating the
+////appropriate type instance using if else condition
+//if (cardType == "MoneyBack")
+//{
+//    cardDetails = new MoneyBack();
+//}
+//else if (cardType == "Titanium")
+//{
+//    cardDetails = new Titanium();
+//}
+//else if (cardType == "Platinum")
+//{
+//    cardDetails = new Platinum();
+//}
+
+//if (cardDetails != null)
+//{
+//    Console.WriteLine("CardType : " + cardDetails.GetCardType());
+//    Console.WriteLine("CreditLimit : " + cardDetails.GetCreditLimit());
+//    Console.WriteLine("AnnualCharge :" + cardDetails.GetAnnualCharge());
+//}
+//else
+//{
+//    Console.Write("Invalid Card Type");
+//}
+
+//Console.ReadLine();
+//////////////////////////////////////////
+
+//using Factory_Method;
+
+//CreditCard cardDetails = CreditCardFactory.GetCreditCard("Platinum");
+
+//if (cardDetails != null)
+//{
+//    Console.WriteLine("CardType : " + cardDetails.GetCardType());
+//    Console.WriteLine("CreditLimit : " + cardDetails.GetCreditLimit());
+//    Console.WriteLine("AnnualCharge :" + cardDetails.GetAnnualCharge());
+//}
+//else
+//{
+//    Console.Write("Invalid Card Type");
+//}
+
+//Console.ReadLine();
+////////////////////////////////////////////
+
+
+using Factory_Method;
+
+Console.WriteLine("Select the payment gateway (PayPal, Stripe, CreditCard): ");
+string gatewayName = Console.ReadLine();
+
+try
+{
+    IPaymentGateway paymentGateway = PaymentGatewayFactory.CreatePaymentGateway(gatewayName);
+    paymentGateway.ProcessPayment(100.00M);  // Example amount
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.ReadKey();
